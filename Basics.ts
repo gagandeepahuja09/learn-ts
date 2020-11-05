3b24cb8079f2cef7dba30dc5052e59a62e14f6c2
gagandeep.ahuja <gagandeep.ahuja@whiteharjr.com>
2020-11-05 23:25:37 +0530

    ts basics initial

---
+++ Basics.ts
@@ -0,0 +1,48 @@
+/* 
+
+    Typescript = Javascript + A Type System
+    TS Type System 
+        1. Help us catch errors(bugs) during development
+            a. The only way to catch err in JS, is to execute the code and get that error appear on the screen.
+            (That's definitely not the most eff. way)  
+        2. Uses 'type annotations' to analyze our code.
+        3. Only active during development.
+        4. JS & Node don't have any idea what TS is.
+        5. Doesn't provide any performance optimization.
+    sudo npm i -g typescript ts-node
+    tsc --> Typescript compiler
+    TS -> TS Compiler -> JS 
+
+    VS Code Shortcuts:
+        1. Add 'code' to path -> Views -> Command Palette -> 'Install code command in path'
+        2. Run prettier on save => Code -> Preferences -> Settings
+        3. Use single quotes with prettier
+        4. Use two spaces for indentation
+
+  #1 First App   
+    * Make a n/w req to fetch some JSON 
+    * Catching errors like mistakenly changing parameter positions 
+    
+ #2 Overview  
+    1st Part: Syntax + Features
+        e.g What is an interface & What is the syntax for defining an interface?
+    2nd Part: Design Patterns With TS
+        How do we use interfaces to write testable code
+    
+    Goal:
+        1. Basic types in TS
+        2. Function typing + annotations
+        3. Type definition files
+        4. Arrays in TS
+        5. Modules systems
+        6. Classes + Refresher on OOP
+        7. Project
+    
+    Steps:
+        1. Def + Overview
+        2. Why do we care? 
+        3. Examples
+        4. When to use this
+    
+    Types: Easy way to refer to the diffrent properties + functions that a value has.
+*/
---
+++ fetchjson/Intro.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
+}
